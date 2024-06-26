26f6d33c0befe3db5944491b06e3e8c275c353dc
Noppol_Somsri <noppolboom@outlook.com>
2024-06-26 12:26:49 +0700

    update

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api/favorite-images'
+// };
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/favorite-images'
+  apiUrl: 'https://favoriteimages-50371aa41031.herokuapp.com/api/favorite-images'
 };
